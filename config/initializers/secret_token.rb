a6a3755cdd157854fe342a6871c6135811c3da42
b <bryceshowell@gmail.com>
2013-08-25 14:49:06 -0400

    CreEATED RAILS APP STATU SUP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'b5704cc1056dcd5970622942451d662e499cb5919e11c5e78a2b79784f4212c6aa0643b9cfd55f29f064050fc38a4b29a1a2c2e1d42f04836b3055ea13225f2e'
